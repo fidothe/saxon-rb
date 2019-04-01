a1a2707ee3d5ec3aba8d2fcd1e98a30f7e0b59f8
Matt Patterson <matt@reprocessed.org>
2019-04-01 12:44:13 +0200

    Move bundled Saxon HE to 9.9.1.2

--- lib/saxon_jars.rb
+++ lib/saxon_jars.rb
@@ -2,9 +2,9 @@
 begin
   require 'jar_dependencies'
 rescue LoadError
-  require 'net/sf/saxon/Saxon-HE/9.8.0-12/Saxon-HE-9.8.0-12.jar'
+  require 'net/sf/saxon/Saxon-HE/9.9.1-2/Saxon-HE-9.9.1-2.jar'
 end
 
 if defined? Jars
-  require_jar 'net.sf.saxon', 'Saxon-HE', '9.8.0-12'
+  require_jar 'net.sf.saxon', 'Saxon-HE', '9.9.1-2'
 end
