8685fa425a21a5cf9dc139e015ce80dbe14a6247
Matt Patterson <matt@reprocessed.org>
2019-09-25 20:40:32 +0200

    Update bundled Saxon HE to 9.9.1.5

--- lib/saxon_jars.rb
+++ lib/saxon_jars.rb
@@ -2,9 +2,9 @@
 begin
   require 'jar_dependencies'
 rescue LoadError
-  require 'net/sf/saxon/Saxon-HE/9.9.1-2/Saxon-HE-9.9.1-2.jar'
+  require 'net/sf/saxon/Saxon-HE/9.9.1-5/Saxon-HE-9.9.1-5.jar'
 end
 
 if defined? Jars
-  require_jar 'net.sf.saxon', 'Saxon-HE', '9.9.1-2'
+  require_jar 'net.sf.saxon', 'Saxon-HE', '9.9.1-5'
 end
