a4fc47ae49d88060e2beba5fd98aadc41ce675ab
Matt Patterson <matt@reprocessed.org>
2019-10-15 12:44:35 +0200

    Add Circle CI config for multiple Saxon versions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,13 @@ require 'vcr'
 require 'simplecov'
 SimpleCov.start
 
+# Handle loading alternate Saxon JARs for testing
+if ENV['ALTERNATE_SAXON_HOME']
+  puts "Alternate Saxon requested at: #{ENV['ALTERNATE_SAXON_HOME']}"
+  require 'saxon/loader'
+  Saxon::Loader.load!(ENV['ALTERNATE_SAXON_HOME'])
+end
+
 require 'saxon/source'
 
 module FixtureHelpers
