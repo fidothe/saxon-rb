abc59c8eac9b9a23e2348dbc0085ac07c4e8c340
Matt Patterson <matt@reprocessed.org>
2020-05-09 21:23:26 +0200

    Add missing documentation

--- lib/saxon/feature_flags/errors.rb
+++ lib/saxon/feature_flags/errors.rb
@@ -1,5 +1,7 @@
 module Saxon
   module FeatureFlags
+    # Error raised if a feature is not available under the loaded version of
+    # Saxon
     class UnavailableInThisSaxonVersionError < StandardError
     end
   end
--- lib/saxon/feature_flags/version.rb
+++ lib/saxon/feature_flags/version.rb
@@ -2,12 +2,31 @@ require_relative '../version/library'
 require_relative 'errors'
 
 module Saxon
-  # Allows saxon-rb features to be switched off if they can't be used under one
-  # of the otherwise-supported Saxon versions
-  #
-  # @api private
   module FeatureFlags
+    # Restrict a specific method to only work with the specified Saxon version
     class Version
+      # Modify the method so that it will only run if the version constraint is
+      # satisfied.
+      #
+      # We can't know what version of Saxon is in use at code load time, only
+      # once {Saxon::Loader#load!} has been called, either explicitly or by
+      # calling a method which requires a Saxon Java object. Therefore, we have
+      # to check this the first time someone calls the method.
+      #
+      # Creating this check replaces the method on the target class with one
+      # that checks the version, and runs the original version if its constraint
+      # is satisfied.
+      #
+      # To avoid performing the check every time the method is called, once we
+      # know whether or not the constraint is satisfied, we assume that the
+      # verion of Saxon cannot be unloaded and a new one loaded in its place and
+      # replace our version checking method with the original (if the constraint
+      # is passed), or with one that simply +raise+s the constraint error.
+      #
+      # @param klass [Class] the class the method lives on
+      # @param method_name [Symbol] the name of the method to be constrained
+      # @param version_constraint [String] the version constraint
+      #   (<tt>'>9.9.1.7'</tt> or <tt>'>= 9.9'</tt>)
       def self.create(klass, method_name, version_constraint)
         method = klass.instance_method(method_name)
         version_check = new(version_constraint)
@@ -26,17 +45,32 @@ module Saxon
         })
       end
 
-      attr_reader :constraint_string, :comparison_operator, :version_string
+      # @return [String] the complete constraint string
+      attr_reader :constraint_string
+      # @return [Symbol] the extracted comparison operator
+      attr_reader :comparison_operator
+      # @return [String] the extracted version string
+      attr_reader :version_string
 
+      # Create a version constraint check from the supplied constraint string
+      #
+      # @param constraint_string [String] the version constraint
       def initialize(constraint_string)
         @constraint_string = constraint_string
         @comparison_operator, @version_string = parse_version_constraint(constraint_string)
       end
 
+      # Reports if the version constraint is satisfied or not.
+      #
+      # @return [Boolean] true if the constraint is satisfied, false otherwise
       def ok?
         Saxon::Version::Library.loaded_version.send(comparison_operator, constraint_version)
       end
 
+      # Generates a {Saxon::Version::Library} representing the version specified
+      # in the constraint that can be compared with the loaded Saxon version
+      #
+      # @return [Saxon::Version::Library] the constraint version
       def constraint_version
         @constraint_version ||= begin
           components = version_string.split('.').map { |n| Integer(n, 10) }
