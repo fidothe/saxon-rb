b12e47b187a5509f3cedacc2bae6e903fcea146b
Matt Patterson <matt@reprocessed.org>
2020-06-24 12:46:52 +0200

    Bump version for release

--- lib/saxon/version.rb
+++ lib/saxon/version.rb
@@ -4,6 +4,6 @@ module Saxon
   # Provides the saxon-rb and underlying Saxon library versions
   module Version
     # The version of the saxon-rb gem (not of Saxon itself)
-    WRAPPER = "0.7.0"
+    WRAPPER = "0.7.1"
   end
 end
