d815f437d074251f8ede6404b9f1bba9bfb39af9
Matt Patterson <matt@reprocessed.org>
2020-07-17 19:40:09 +0200

    Expand mutex use during const loading
    Was seeing const redefinition warnings when using multiple threads, this
    tightens things up.

--- lib/saxon/s9api.rb
+++ lib/saxon/s9api.rb
@@ -11,28 +11,28 @@ module Saxon
       # alternate location for them, if they don't want to use the bundled Saxon
       # HE
       def const_missing(name)
-        Saxon::Loader.load!
-        begin
-          const_set(name, imported_classes.const_get(name))
-        rescue NameError
-          msg = "uninitialized constant Saxon::S9API::#{name}"
-          e = NameError.new(msg, name)
-          raise e
-        end
+        CLASS_IMPORT_SEMAPHORE.synchronize {
+          return const_get(name) if const_defined?(name)
+          Saxon::Loader.load!
+          begin
+            const_set(name, imported_classes.const_get(name))
+          rescue NameError
+            msg = "uninitialized constant Saxon::S9API::#{name}"
+            e = NameError.new(msg, name)
+            raise e
+          end
+        }
       end
 
       private
 
       def imported_classes
-        return @imported_classes if instance_variable_defined?(:@imported_classes)
-        CLASS_IMPORT_SEMAPHORE.synchronize do
-          @imported_classes = Module.new {
-            include_package 'net.sf.saxon.s9api'
-            java_import Java::net.sf.saxon.Configuration
-            java_import Java::net.sf.saxon.lib.FeatureKeys
-            java_import Java::net.sf.saxon.lib.ParseOptions
-          }
-        end
+        @imported_classes ||= Module.new {
+          include_package 'net.sf.saxon.s9api'
+          java_import Java::net.sf.saxon.Configuration
+          java_import Java::net.sf.saxon.lib.FeatureKeys
+          java_import Java::net.sf.saxon.lib.ParseOptions
+        }
       end
     end
   end
