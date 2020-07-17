5d11f1a65945f207de39c6640d0af99e5b345d7c
Matt Patterson <matt@reprocessed.org>
2020-07-17 19:41:18 +0200

    Work around JRuby #6197
    Thanks to @headius for the suggestion

---
+++ lib/saxon/jruby_bug_6197_workaround.rb
@@ -0,0 +1,5 @@
+if Java::net::sf::saxon::s9api.const_defined?(:AbstractXsltTransformer)
+  class Java::net::sf::saxon::s9api::AbstractXsltTransformer
+    java_alias :setInitialMode, :setInitialMode, [Java::net::sf::saxon::s9api::QName]
+  end
+end
